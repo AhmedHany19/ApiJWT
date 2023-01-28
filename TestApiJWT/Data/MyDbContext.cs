14dccd38b4d784ae87bc82c66f0019f45cbfdcc5
WilzY <ahmedwilzy@gmail.com>
2023-01-28 22:43:40 +0200

    Add project files.

---
+++ TestApiJWT/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using TestApiJWT.Models;
+
+namespace TestApiJWT.Data
+{
+    public class MyDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
---
+++ TestApiJWT/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using TestApiJWT.Models;
+
+namespace TestApiJWT.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+
+    }
+}
